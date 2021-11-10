2c145ed61ad856d453aa1ffa6d279f753d37f7ff
prf-alekie <gichualex47@gmail.com>
2021-11-10 20:13:54 +0300

    update

--- home.php
+++ home.php
@@ -1,168 +1,221 @@
-<style>
-	.custom-menu {
-        z-index: 1000;
-	    position: absolute;
-	    background-color: #ffffff;
-	    border: 1px solid #0000001c;
-	    border-radius: 5px;
-	    padding: 8px;
-	    min-width: 13vw;
-}
-a.custom-menu-list {
-    width: 100%;
-    display: flex;
-    color: #4c4b4b;
-    font-weight: 600;
-    font-size: 1em;
-    padding: 1px 11px;
-}
-	span.card-icon {
-    position: absolute;
-    font-size: 3em;
-    bottom: .2em;
-    color: #ffffff80;
-}
-.file-item{
-	cursor: pointer;
-}
-a.custom-menu-list:hover,.file-item:hover,.file-item.active {
-    background: #80808024;
-}
-table th,td{
-	/*border-left:1px solid gray;*/
-}
-a.custom-menu-list span.icon{
-		width:1em;
-		margin-right: 5px
-}
-.candidate {
-    margin: auto;
-    width: 23vw;
-    padding: 0 10px;
-    border-radius: 20px;
-    margin-bottom: 1em;
-    display: flex;
-    border: 3px solid #00000008;
-    background: #8080801a;
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
 
-}
-.candidate_name {
-    margin: 8px;
-    margin-left: 3.4em;
-    margin-right: 3em;
-    width: 100%;
-}
-	.img-field {
-	    display: flex;
-	    height: 8vh;
-	    width: 4.3vw;
-	    padding: .3em;
-	    background: #80808047;
-	    border-radius: 50%;
-	    position: absolute;
-	    left: -.7em;
-	    top: -.7em;
-	}
-	
-	.candidate img {
-    height: 100%;
-    width: 100%;
-    margin: auto;
-    border-radius: 50%;
-}
-.vote-field {
-    position: absolute;
-    right: 0;
-    bottom: -.4em;
-}
-</style>
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
 
-<div class="containe-fluid">
-	<?php include('db_connect.php') ;
-	$voting = $conn->query("SELECT * FROM voting_list where  is_default = 1 ");
-	foreach ($voting->fetch_array() as $key => $value) {
-		$$key = $value;
-	}
-	$votes  = $conn->query("SELECT * FROM votes where voting_id = $id ");
-	$v_arr = array();
-	while($row=$votes->fetch_assoc()){
-		if(!isset($v_arr[$row['voting_opt_id']]))
-			$v_arr[$row['voting_opt_id']] = 0;
+	      <!-- Main content -->
+	      <section class="content">
+	      	<?php
+	      		$parse = parse_ini_file('admin/config.ini', FALSE, INI_SCANNER_RAW);
+    			$title = $parse['election_title'];
+	      	?>
+	      	<h1 class="page-header text-center title"><b><?php echo strtoupper($title); ?></b></h1>
+	        <div class="row">
+	        	<div class="col-sm-10 col-sm-offset-1">
+	        		<?php
+				        if(isset($_SESSION['error'])){
+				        	?>
+				        	<div class="alert alert-danger alert-dismissible">
+				        		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+					        	<ul>
+					        		<?php
+					        			foreach($_SESSION['error'] as $error){
+					        				echo "
+					        					<li>".$error."</li>
+					        				";
+					        			}
+					        		?>
+					        	</ul>
+					        </div>
+				        	<?php
+				         	unset($_SESSION['error']);
 
-		$v_arr[$row['voting_opt_id']] += 1;
-	}
-	$opts = $conn->query("SELECT * FROM voting_opt where voting_id=".$id);
-	$opt_arr = array();
-		while($row=$opts->fetch_assoc()){
-		$opt_arr[$row['category_id']][] = $row;
+				        }
+				        if(isset($_SESSION['success'])){
+				          	echo "
+				            	<div class='alert alert-success alert-dismissible'>
+				              		<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
+				              		<h4><i class='icon fa fa-check'></i> Success!</h4>
+				              	".$_SESSION['success']."
+				            	</div>
+				          	";
+				          	unset($_SESSION['success']);
+				        }
 
-	}
+				    ?>
+ 
+				    <div class="alert alert-danger alert-dismissible" id="alert" style="display:none;">
+		        		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+			        	<span class="message"></span>
+			        </div>
 
-	?>
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="card col-md-4 offset-2 bg-info float-left">
-				<div class="card-body text-white">
-					<h4><b>Voters</b></h4>
-					<hr>
-					<span class="card-icon"><i class="fa fa-users"></i></span>
-					<h3 class="text-right"><b><?php echo $conn->query('SELECT * FROM users where type = 2 ')->num_rows ?></b></h3>
-				</div>
-			</div>
-			<div class="card col-md-4 offset-2 bg-primary ml-4 float-left">
-				<div class="card-body text-white">
-					<h4><b>Voted</b></h4>
-					<hr>
-					<span class="card-icon"><i class="fa fa-user-tie"></i></span>
-					<h3 class="text-right"><b><?php echo $conn->query('SELECT distinct(user_id) FROM votes where voting_id = '.$id)->num_rows ?></b></h3>
-				</div>
-			</div>
-		</div>
-	</div>
+				    <?php
+				    	$sql = "SELECT * FROM votes WHERE voters_id = '".$voter['id']."'";
+				    	$vquery = $conn->query($sql);
+				    	if($vquery->num_rows > 0){
+				    		?>
+				    		<div class="text-center">
+					    		<h3>You have already voted for this election.</h3>
+					    		<a href="#view" data-toggle="modal" class="btn btn-flat btn-primary btn-lg">View Ballot</a>
+					    	</div>
+				    		<?php
+				    	}
+				    	else{
+				    		?>
+			    			<!-- Voting Ballot -->
+						    <form method="POST" id="ballotForm" action="submit_ballot.php">
+				        		<?php
+				        			include 'includes/slugify.php';
 
-	<div class="row mt-3 ml-3 mr-3">
-			<div class="col-lg-12">
-			<div class="card">
-				<div class="card-body">
-					<div class="text-center">
-						<h3><b><?php echo $title ?></b></h3>
-						<small><b><?php echo $description; ?></b></small>	
-					</div>
-					<?php 
-					$cats = $conn->query("SELECT * FROM category_list where id in (SELECT category_id from voting_opt where voting_id = '".$id."' )");
-					while($row = $cats->fetch_assoc()):
-					?>
-						<hr>
-						<div class="row mb-4">
-							<div class="col-md-12">
-									<div class="text-center">
-										<h3><b><?php echo $row['category'] ?></b></h3>
-									</div>
-							</div>
-						</div>
-						<div class="row mt-3">
-						<?php foreach ($opt_arr[$row['id']] as $candidate) {
-						?>
-							<div class="candidate" style="position: relative;">
-								<div class="img-field">
-									<img src="assets/img/<?php echo $candidate['image_path'] ?>" alt="">
-								</div>
-								<div class="candidate_name"><?php echo $candidate['opt_txt'] ?></div>
-								<div class="vote-field">
-									<span class="badge badge-success"><large><b><?php echo isset($v_arr[$candidate['id']]) ? number_format($v_arr[$candidate['id']]) : 0 ?></b></large></span>
-								</div>
-							</div>
-						<?php } ?>
-						</div>
-					<?php endwhile; ?>					
-				</div>
-			</div>
-			
-		</div>
-		</div>
-	</div>
+				        			$candidate = '';
+				        			$sql = "SELECT * FROM positions ORDER BY priority ASC";
+									$query = $conn->query($sql);
+									while($row = $query->fetch_assoc()){
+										$sql = "SELECT * FROM candidates WHERE position_id='".$row['id']."'";
+										$cquery = $conn->query($sql);
+										while($crow = $cquery->fetch_assoc()){
+											$slug = slugify($row['description']);
+											$checked = '';
+											if(isset($_SESSION['post'][$slug])){
+												$value = $_SESSION['post'][$slug];
 
+												if(is_array($value)){
+													foreach($value as $val){
+														if($val == $crow['id']){
+															$checked = 'checked';
+														}
+													}
+												}
+												else{
+													if($value == $crow['id']){
+														$checked = 'checked';
+													}
+												}
+											}
+											$input = ($row['max_vote'] > 1) ? '<input type="checkbox" class="flat-red '.$slug.'" name="'.$slug."[]".'" value="'.$crow['id'].'" '.$checked.'>' : '<input type="radio" class="flat-red '.$slug.'" name="'.slugify($row['description']).'" value="'.$crow['id'].'" '.$checked.'>';
+											$image = (!empty($crow['photo'])) ? 'images/'.$crow['photo'] : 'images/profile.jpg';
+											$candidate .= '
+												<li>
+													'.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['firstname'].' '.$crow['lastname'].'"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['firstname'].' '.$crow['lastname'].'</span>
+												</li>
+											';
+										}
+
+										$instruct = ($row['max_vote'] > 1) ? 'You may select up to '.$row['max_vote'].' candidates' : 'Select only one candidate';
+
+										echo '
+											<div class="row">
+												<div class="col-xs-12">
+													<div class="box box-solid" id="'.$row['id'].'">
+														<div class="box-header with-border">
+															<h3 class="box-title"><b>'.$row['description'].'</b></h3>
+														</div>
+														<div class="box-body">
+															<p>'.$instruct.'
+																<span class="pull-right">
+																	<button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
+																</span>
+															</p>
+															<div id="candidate_list">
+																<ul>
+																	'.$candidate.'
+																</ul>
+															</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										';
+
+										$candidate = '';
+
+									}	
+
+				        		?>
+				        		<div class="text-center">
+					        		<button type="button" class="btn btn-success btn-flat" id="preview"><i class="fa fa-file-text"></i> Preview</button> 
+					        		<button type="submit" class="btn btn-primary btn-flat" name="vote"><i class="fa fa-check-square-o"></i> Submit</button>
+					        	</div>
+				        	</form>
+				        	<!-- End Voting Ballot -->
+				    		<?php
+				    	}
+
+				    ?>
+
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+  	<?php include 'includes/ballot_modal.php'; ?>
 </div>
+
+<?php include 'includes/scripts.php'; ?>
 <script>
-	
+$(function(){
+	$('.content').iCheck({
+		checkboxClass: 'icheckbox_flat-green',
+		radioClass: 'iradio_flat-green'
+	});
+
+	$(document).on('click', '.reset', function(e){
+	    e.preventDefault();
+	    var desc = $(this).data('desc');
+	    $('.'+desc).iCheck('uncheck');
+	});
+
+	$(document).on('click', '.platform', function(e){
+		e.preventDefault();
+		$('#platform').modal('show');
+		var platform = $(this).data('platform');
+		var fullname = $(this).data('fullname');
+		$('.candidate').html(fullname);
+		$('#plat_view').html(platform);
+	});
+
+	$('#preview').click(function(e){
+		e.preventDefault();
+		var form = $('#ballotForm').serialize();
+		if(form == ''){
+			$('.message').html('You must vote atleast one candidate');
+			$('#alert').show();
+		}
+		else{
+			$.ajax({
+				type: 'POST',
+				url: 'preview.php',
+				data: form,
+				dataType: 'json',
+				success: function(response){
+					if(response.error){
+						var errmsg = '';
+						var messages = response.message;
+						for (i in messages) {
+							errmsg += messages[i]; 
+						}
+						$('.message').html(errmsg);
+						$('#alert').show();
+					}
+					else{
+						$('#preview_modal').modal('show');
+						$('#preview_body').html(response.list);
+					}
+				}
+			});
+		}
+		
+	});
+
+});
 </script>
+</body>
+</html>
--- login.php
+++ login.php
@@ -1,102 +1,32 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
--->
-<!DOCTYPE html>
-<html lang="zxx">
-<?php include('./header.php'); ?>
-<?php 
-session_start();
-if(isset($_SESSION['login_id']))
-header("location:index.php?page=home");
-?>
+<?php
+	session_start();
+	include 'includes/conn.php';
 
+	if(isset($_POST['login'])){
+		$voter = $_POST['voter'];
+		$password = $_POST['password'];
 
-<head>
-    <title>Gathering Log In form Responsive Widget Template :: W3layouts</title>
-    <!-- Meta tag Keywords -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta charset="UTF-8" />
-    <!-- //Meta tag Keywords -->
-    <link href="//fonts.googleapis.com/css2?family=Kumbh+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-    <!--/Style-CSS -->
-    <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
-    <!--//Style-CSS -->
-    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" media="all">
+		$sql = "SELECT * FROM voters WHERE voters_id = '$voter'";
+		$query = $conn->query($sql);
 
-</head>
-
-<body>
-    <div class="w3l-signinform">
-        <!-- container -->
-        <div class="wrapper">
-            <!-- main content -->
-            <div class="w3l-form-info">
-                <div class="w3_info">
-                    <h1>Welcome Back</h1>
-                    <p class="sub-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    <h2>Log In</h2>
-                    <form action="#" method="post">
-                        <div class="input-group">
-                            <span><i class="fa fa-user" aria-hidden="true"></i></span>
-                            <input type="email" placeholder="Email or Username" required="">
-                        </div>
-                        <div class="input-group two-groop">
-                            <span><i class="fa fa-key" aria-hidden="true"></i></span>
-                            <input type="Password" placeholder="Password" required="">
-                        </div>
-                        <div class="form-row bottom">
-                            <div class="form-check">
-                                <input type="checkbox" id="remenber" name="remenber" value="remenber">
-                                <label for="remenber"> Remember me?</label>
-                            </div>
-                            <a href="#url" class="forgot">Forgot password?</a>
-                        </div>
-                        <button class="btn btn-primary btn-block" type="submit">Log In</button>
-                    </form>
-                
-                    <p class="account">Don't have an account? <a href="#register">Register</a></p>
-                </div>
-            </div>
-            <!-- //main content -->
-        </div>
-        <!-- //container -->
-        <!-- footer -->
-        <div class="footer">
-            <p>&copy; 2021 Gathering Log In form. All Rights Reserved | Design by <a href="https://w3layouts.com/"
-                    target="blank">W3layouts</a></p>
-        </div>
-        <!-- footer -->
-    </div>
-
-</body>
-<script>
-	$('#login-form').submit(function(e){
-		e.preventDefault()
-		$('#login-form button[type="button"]').attr('disabled',true).html('Logging in...');
-		if($(this).find('.alert-danger').length > 0 )
-			$(this).find('.alert-danger').remove();
-		$.ajax({
-			url:'ajax.php?action=login',
-			method:'POST',
-			data:$(this).serialize(),
-			error:err=>{
-				console.log(err)
-		$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
-
-			},
-			success:function(resp){
-				if(resp == 1){
-					location.href ='index.php?page=home';
-				}else if(resp == 2){
-					location.href ='voting.php';
-				}else{
-					$('#login-form').prepend('<div class="alert alert-danger">Username or password is incorrect.</div>')
-					$('#login-form button[type="button"]').removeAttr('disabled').html('Login');
-				}
+		if($query->num_rows < 1){
+			$_SESSION['error'] = 'Cannot find voter with the ID';
+		}
+		else{
+			$row = $query->fetch_assoc();
+			if(password_verify($password, $row['password'])){
+				$_SESSION['voter'] = $row['id'];
 			}
-		})
-	})
-</script>	
+			else{
+				$_SESSION['error'] = 'Incorrect password';
+			}
+		}
+		
+	}
+	else{
+		$_SESSION['error'] = 'Input voter credentials first';
+	}
 
-</html>
+	header('location: index.php');
+
+?>
